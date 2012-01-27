91209405d9a81890140388a29c1b4487e0ab8753
Iwan Budi Kusnanto <iwan.b.kusnanto@gmail.com>
2012-01-27 11:23:38 +0700

    initial support untuk multiple session

--- roomah/client.py
+++ roomah/client.py
@@ -1,5 +1,6 @@
 import sys
 import socket
+import select
 
 import packet
 import mysock
@@ -7,6 +8,89 @@ import mysock
 SERV_BUF_LEN = 1024
 HOST_BUF_LEN = SERV_BUF_LEN - packet.MIN_HEADER_LEN
 
+host_conns_dict = {}
+class HostConn:
+    def __init__(self, ses_id):
+        self.sock = None
+        self.ses_id = ses_id
+
+def get_host_conn_by_sock(sock):
+    if sock == None:
+        return None
+    
+    for k,v in host_conns_dict.iteritems():
+        h_conn = v
+        if h_conn.sock == sock:
+            return h_conn
+    
+    return None
+def forward_incoming_req_pkt(ba, ba_len):
+    req = packet.DataReq(ba)
+    if req.cek_valid() == False:
+        print "bukan DATA REQ"
+        print "FATAL ERROR"
+        sys.exit(-1)
+    
+    ses_id = req.get_sesid()
+    req_data = req.get_data()
+    print req_data
+    
+    h_conn = HostConn(ses_id)
+    host_conns_dict[ses_id] = h_conn
+    
+    #forward ke host
+    h_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    h_conn.sock = h_sock
+    
+    ret, err = mysock.connect(h_sock, (host_host, host_port))
+    written, err = mysock.send(h_sock, req_data)
+    if err != None:
+        print "error forward"
+        del host_conns_dict[ses_id]
+        sys.exit(-1)
+    
+    if written != len(req_data):
+        print "PARTIAL FORWARD to host"
+        print "FATAL UNHANDLED COND"
+        sys.exit(-1)
+        
+def forward_incoming_host_rsp(h_sock, server_sock):
+    h_conn = get_host_conn_by_sock(h_sock)
+    if h_conn == None:
+        print "can't get h_conn by sock"
+        print "FATAL UNHANDLED CONDITION"
+        sys.exi(-1)
+        
+    ba_len, ba, err = mysock.recv(h_sock, HOST_BUF_LEN)
+    count = 0
+    while err == None:
+        rsp_pkt = packet.DataRsp()
+        
+        print "build rsp pkt.ses_id =", h_conn.ses_id
+        
+        rsp_pkt.build(ba, h_conn.ses_id)
+        if ba_len == 0:
+            rsp_pkt.set_eof()
+            
+        written, err = mysock.send(server_sock, rsp_pkt.payload)
+        if err != None:
+            print "error sending packet to server"
+            sys.exit(-1)
+        
+        if written != len(rsp_pkt.payload):
+            print "partial write to server"
+            sys.exit(-1)
+        
+        print "written = ", written
+        
+        if ba_len == 0:
+            print "EOF.exit"
+            break
+        
+        count += 1
+        if count == 5:
+            break
+        ba_len, ba, err = mysock.recv(h_sock, HOST_BUF_LEN)
 if __name__ == '__main__':
     server = sys.argv[1]
     port = int(sys.argv[2])
@@ -18,9 +102,9 @@ if __name__ == '__main__':
     sock_dict = {}
     
     #connect to server
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    mysock.setkeepalives(sock)
-    ret, err = mysock.connect(sock, (server, port))
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mysock.setkeepalives(server_sock)
+    ret, err = mysock.connect(server_sock, (server, port))
     
     if err != None:
         print "can't connect to server"
@@ -29,7 +113,7 @@ if __name__ == '__main__':
     #send auth req
     pkt = packet.Packet(packet.TYPE_AUTH_REQ)
     pkt.auth_req_build(user, passwd)
-    written, err = mysock.send(sock, pkt.payload)
+    written, err = mysock.send(server_sock, pkt.payload)
     if err != None:
         print "can't send auth req to client"
         print "err = ", err
@@ -37,7 +121,7 @@ if __name__ == '__main__':
     print "pkt len = ", len(pkt.payload)
     print "sent len = ", written
     
-    recvd, ba, err = mysock.recv(sock, 1024)
+    recvd, ba, err = mysock.recv(server_sock, 1024)
     if err != None:
         print "failed to get auth reply"
         sys.exit(-1)
@@ -53,42 +137,36 @@ if __name__ == '__main__':
         sys.exit(-1)
     
     print "AUTH OK"
-    str_len, str, err = mysock.recv_str(sock, 1024)
-    if err != None:
-        print "error receiving data packet"
-        sys.exit(-1)
     
+    #start looping
+    while True:
+        #select untuk server sock
+        to_read, to_write, to_exc = select.select([server_sock], [], [], 0.1)
+        if len(to_read) > 0:
+            #read sock
+            ba_len, ba, err = mysock.recv(server_sock, 1024)
+            if err != None:
+                print "error receiving data packet"
+                sys.exit(-1)
+            if ba_len == 0:
+                print "server closed the connection"
+                print "FATAL UNHANDLED Condition"
+                sys.exit(-1)
+                
+            forward_incoming_req_pkt(ba, ba_len)
+        
+        if len(to_write) > 0:
+            pass
+        
+        #select untuk host sock
+        rlist = []
+        for k,v in host_conns_dict.iteritems():
+            h_conn = v
+            rlist.append(h_conn.sock)
+        h_read, h_write, h_exc = select.select(rlist, [], [], 0.1)
+        if len(h_read) > 0:
+            for s in h_read:
+                forward_incoming_host_rsp(s, server_sock)
+                
     print "###########"
-    req_pkt = packet.Packet(packet.TYPE_DATA_REQ, bytearray(str))
-    if req_pkt.data_req_cek() == False:
-        print "bukan DATA REQ"
-        print "FATAL ERROR"
-        sys.exit(-1)
     
-    ses_id = req_pkt.data_req_get_sesid()
-    real_req = req_pkt.data_req_get_data()
-    print real_req
-    #forward ke host
-    h_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ret, err = mysock.connect(h_sock, (host_host, host_port))
-    written, err = mysock.send(h_sock, real_req)
-    if err != None:
-        print "error forward"
-        sys.exit(-1)
-    
-    ba_len, ba, err = mysock.recv(h_sock, HOST_BUF_LEN)
-    while err == None:
-        rsp_pkt = packet.Packet()
-        print "build rsp pkt.ses_id =", ses_id
-        rsp_pkt.data_rsp_build(ba, ses_id, 0)
-        written, err = mysock.send(sock, rsp_pkt.payload)
-        if err != None:
-            print "error sending packet to server"
-            sys.exit(-1)
-        
-        if written != len(rsp_pkt.payload):
-            print "partial write to server"
-            sys.exit(-1)
-        
-        print "written = ", written
-        ba_len, ba, err = mysock.recv(h_sock, HOST_BUF_LEN)
