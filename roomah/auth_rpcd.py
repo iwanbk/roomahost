991a6412b8b7d760ad726194621aabcea1fb9daa
Iwan Budi Kusnanto <iwan.b.kusnanto@gmail.com>
2012-01-28 05:05:33 +0700

    simple auth server

---
+++ roomah/auth_rpcd.py
@@ -0,0 +1,20 @@
+from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
+
+RES_OK = 1
+RES_UNKNOWN_ERR = 0
+RES_USER_NOT_FOUND = -1
+RES_WRONG_PASS = -2
+RES_EXCEED_QUOTA = -3
+
+def auth_client(username, password):
+    print "username = ", username
+    print "password = ", username
+    if username == password:
+        return RES_OK
+    
+    return RES_UNKNOWN_ERR
+
+if __name__ == '__main__':
+    server = SimpleJSONRPCServer(('0.0.0.0',4141 ))
+    server.register_function(auth_client)
+    server.serve_forever()
