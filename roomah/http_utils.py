9b7e2c12f36f01109669268f9509d285db33016d
Iwan Budi Kusnanto <iwan.b.kusnanto@gmail.com>
2012-01-28 03:52:46 +0700

    HTTP utils

---
+++ roomah/http_utils.py
@@ -0,0 +1,18 @@
+from BaseHTTPServer import BaseHTTPRequestHandler
+from StringIO import StringIO
+
+class HTTPRequest(BaseHTTPRequestHandler):
+    def __init__(self, request_text):
+        self.rfile = StringIO(request_text)
+        self.raw_requestline = self.rfile.readline()
+        self.error_code = self.error_message = None
+        self.parse_request()
+
+    def send_error(self, code, message):
+        self.error_code = code
+        self.error_message = message
+
+def get_http_req_header(request):
+    '''Get HTTP Request headers.'''
+    http_req = HTTPRequest(request)
+    return http_req.headers
