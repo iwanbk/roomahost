46599d92b68b3b7efef632e2577464626f1ababf
Iwan Budi Kusnanto <iwan.b.kusnanto@gmail.com>
2012-02-08 14:32:09 +0700

    Pindah semua code client ke client.py Pindah semua code peer ke peer.py

--- roomah/client.py
+++ roomah/client.py
@@ -1,12 +1,23 @@
 import sys
+import select
 
 import gevent
+from gevent.server import StreamServer
+import jsonrpclib
 
 from peer import Peer
 import packet
 import mysock
+import auth_rpcd
+from client_mgr import ClientMgr
+
+AUTH_RES_OK = 1
+AUTH_RES_UNKNOWN_ERR = 0
+
+auth_server = jsonrpclib.Server('http://localhost:4141')
 
 BUF_LEN = 1024
+CM = None
 
 class ReqPkt:
     def __init__(self, peer, payload):
@@ -164,3 +175,114 @@ class Client:
         msg['pkt'] = rsp
         
         peer_mq.put(msg)
+
+
+def client_auth_reply(sock):
+    pass
+
+def client_auth_rpc(username, password):
+    res = auth_server.auth_client(str(username), str(password))
+    return res
+
+def client_auth(sock):
+    #get auth req
+    ba, err = mysock.recv(sock, BUF_LEN)
+    if err != None:
+        print "can't recv auth req"
+    
+    pkt = packet.Packet(None, ba)
+    
+    if pkt.auth_req_cek() == False:
+        print "Bukan Paket AUTH REQ"
+        sys.exit(-1)
+    
+    user, password = pkt.auth_req_get_userpassword()
+    print "user = ", user
+    print "password = ", password
+    
+    if client_auth_rpc(user, password) != auth_rpcd.RES_OK:
+        print "auth error"
+        return user, AUTH_RES_UNKNOWN_ERR
+    
+    rsp = packet.Packet(packet.TYPE_AUTH_RSP)
+    rsp.auth_rsp_build(packet.AUTH_RSP_OK)
+    
+    written, err = mysock.send_all(sock, rsp.payload)
+    if err != None or written != len(rsp.payload):
+        print "can't send auth reply"
+        return user, AUTH_RES_UNKNOWN_ERR
+    
+    return user, AUTH_RES_OK
+
+def unregister_client(CM, client):
+    msg = {}
+    msg['mt'] = CM.MT_CLIENT_DEL_REQ
+    msg['user'] = client.user
+    
+    CM.in_mq.put(msg)
+    
+def register_client(CM, user, in_mq):
+    #prepare the message
+    msg = {}
+    msg['mt'] = ClientMgr.MT_CLIENT_ADD_REQ
+    msg['user'] = user
+    msg['in_mq'] = in_mq
+    
+    #send the message
+    CM.in_mq.put(msg)
+    
+    #wait the reply
+    return True
+    
+def handle_client(sock, addr):
+    print "sock = ", sock
+    print "addr = ", addr
+    
+    user, auth_res = client_auth(sock)
+    if auth_res != AUTH_RES_OK:
+        print "AUTH failed"
+        return
+    
+    cli = Client(user, sock)
+    register_client(CM, cli.user, cli.in_mq)
+    
+    while True:
+        cli.process_msg()
+        
+        #select() sock
+        wlist = []
+        if len(cli.req_pkt) > 0 or cli.wait_ping_rsp == True:
+            wlist.append(sock)
+            
+            
+        rsocks, wsocks, _ = select.select([sock], wlist , [], 0.1)
+        if len(rsocks) > 0:       
+            ba, err = packet.get_all_data_pkt(sock)
+            if ba is None or err is not None:
+                print "read client sock err.exiting.."
+                break
+            
+            if ba[0] == packet.TYPE_DATA_RSP:  
+                cli.procsess_rsp_pkt(ba, len(ba))
+            elif ba[0] == packet.TYPE_PING_REQ:
+                #print "[PING-REQ] from ", client.user
+                cli.wait_ping_rsp = True
+        
+        
+        if len(wsocks) > 0:
+            #forward http request packet to client
+            if cli.req_pkt_fwd(1) == False:
+                break
+            #send PING-RSP to client
+            if cli.ping_rsp_send() == False:
+                break
+
+        gevent.sleep(0)
+    
+    #CM.del_client(client)
+    unregister_client(CM, cli)
+
+def client_server(port):
+    server = StreamServer(('0.0.0.0', port), handle_client)
+    print 'Starting client server on port ', port
+    server.serve_forever()
--- roomah/peer.py
+++ roomah/peer.py
@@ -1,12 +1,25 @@
 """
-Class yang merepresentasikan satu Peer
-Peer adalah sebuah koneksi dari pengakses (misal : browser)
+Peer module
 """
+import select
+
 import gevent
+from gevent.server import StreamServer
 
 import mysock
+import client
+import http_utils
+from client_mgr import ClientMgr
+
+BASE_DOMAIN = ""
+CM = None
+BUF_LEN = 1024
 
 class Peer:
+    """
+    Class yang merepresentasikan satu Peer
+    Peer adalah sebuah koneksi dari pengakses (misal : browser)
+    """
     RSP_FORWARD_NUM = 5
     MT_ADD_RSP_PKT = 1
     
@@ -57,3 +70,150 @@ class Peer:
             written = self._do_forward_rsp_pkt()
             if written <= 0:
                 break
+
+def user_not_found_str(user):
+    html = "HTTP/1.1\n\
+Server: nginx/1.1.11\n\
+Content-Type: text/html\n\
+Content-Length: 173\n\
+Connection: close\n\
+\r\n\
+"
+    html += user + " ga ketemu"
+    return html
+
+def get_subdom(req, base_domain=BASE_DOMAIN):
+    header = http_utils.get_http_req_header(req)
+        
+    host = header['Host']
+    idx = host.find("." + base_domain)
+    if idx < 0:
+        return None
+    else:
+        return host[:idx]
+
+def get_client_mq(CM, subdom):
+    q = gevent.queue.Queue(1)
+    msg = {}
+    msg['mt'] = ClientMgr.MT_CLIENT_GET_REQ
+    msg['user_str'] = subdom
+    msg['q'] = q
+    
+    CM.in_mq.put(msg)
+    
+    rsp = q.get()
+    client_mq = rsp['client_mq']
+    return client_mq
+    
+def _add_peer_to_client(client_mq, sock):
+    peer = Peer(sock, None)
+    
+    q = gevent.queue.Queue(1)
+    msg = {}
+    msg['mt'] = client.Client.MT_PEER_ADD_REQ
+    msg['in_mq'] = peer.in_mq
+    msg['q'] = q
+    client_mq.put(msg)
+    
+    rsp = q.get()
+    
+    ses_id = rsp['ses_id']
+    
+    peer.ses_id = ses_id
+    
+    return peer
+
+def _peer_add_reqpkt_to_client(client_mq, req_pkt):
+    msg = {}
+    msg['mt'] = client.Client.MT_REQPKT_ADD_REQ
+    msg['req_pkt'] = req_pkt
+    client_mq.put(msg)
+
+def _peer_del_from_client(client_mq, peer):
+    msg = {}
+    msg['mt'] = client.Client.MT_PEER_DEL_REQ
+    msg['ses_id'] = peer.ses_id
+    client_mq.put(msg)
+    
+def handle_peer(sock, addr):
+    #print "##### peer baru ############"
+    #print "sock = ", sock
+    #print "addr = ", addr
+    
+    ba, err = mysock.recv(sock, BUF_LEN)
+    if err != None:
+        print "recv error"
+        return
+    
+    if len(ba) == 0:
+        print "new-closed socket?"
+        return
+    
+    subdom = get_subdom(ba, BASE_DOMAIN)
+    
+    if subdom is None:
+        print "subdom not found"
+        sock.close()
+        return
+    
+    client_mq = get_client_mq(CM, subdom)
+    
+    if client_mq == None:
+        print "send user error"
+        str_err = user_not_found_str(subdom)
+        mysock.send_all(sock, str_err)
+        return
+    
+    peer = _add_peer_to_client(client_mq, sock)
+    
+    if peer == None:
+        print "can't add peer. MAX_CONN REACHED?"
+        return
+    
+    #print "peer baru.ses_id =", peer.ses_id
+    req_pkt = client.ReqPkt(peer, ba)
+    _peer_add_reqpkt_to_client(client_mq, req_pkt)
+    
+    while True:
+        wlist = []
+        
+        try:
+            for _ in xrange(0, Peer.RSP_FORWARD_NUM):
+                msg = peer.in_mq.get_nowait()
+                rsp = msg['pkt']
+                peer.rsp_list.append(rsp)
+        except gevent.queue.Empty:
+            pass
+        
+        if len(peer.rsp_list) > 0:
+            wlist.append(sock)
+            
+        rsocks, wsocks, _ = select.select([sock], wlist , [], 0.1)
+        
+        if len(rsocks) > 0:
+            #TODO : buat fungsi sendiri . seperti peer.forward_rsp_pkt, di-loop
+            ba, err = mysock.recv(sock, BUF_LEN)
+            if len(ba) == 0:
+                #peer close the socket
+                peer.close()
+                _peer_del_from_client(client_mq, peer)
+                break    
+            elif len(ba) > 0:
+                req_pkt = client.ReqPkt(peer, ba)
+                _peer_add_reqpkt_to_client(client_mq, req_pkt)
+        
+        if len(wsocks) > 0:
+            peer.forward_rsp_pkt()
+        
+        if peer.ended == True and len(peer.rsp_list) == 0:
+            peer.close()
+            _peer_del_from_client(client_mq, peer)
+            break
+        
+        gevent.sleep(0)
+        
+def peer_server(port):
+    server = StreamServer(('0.0.0.0', port), handle_peer)
+    print 'Starting peer server on port ', port
+    print 'BASE_DOMAIN = ', BASE_DOMAIN
+    server.serve_forever()
